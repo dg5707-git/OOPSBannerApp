97c7f069e5fe8c705e81d23cc6699f3688355453
dg5707-git <dg5707@srmist.edu.in>
2026-03-07 18:48:05 +0530

    feat: Added OOPSBannerApp using textart

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,1 +1,21 @@
 
+/*
+ * @author: Dhruv Kumar Gupta
+ * @version: 1.0
+ *
+ *
+ */
+
+public class OOPSBannerApp_2 {
+    public static void main(String[] args) {
+
+        System.out.println(" *****     *****    ******     *****  ");
+        System.out.println("*     *   *     *   *     *   *     * ");
+        System.out.println("*     *   *     *   *     *   *       ");
+        System.out.println("*     *   *     *   ******     *****  ");
+        System.out.println("*     *   *     *   *              *  ");
+        System.out.println("*     *   *     *   *        *     *  ");
+        System.out.println(" *****     *****    *         *****   ");
+
+    }
+}
