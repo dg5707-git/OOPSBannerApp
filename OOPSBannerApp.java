d71f77eaac3a9b1137f1f24269daa4e6a0c3f775
dg5707-git <dg5707@srmist.edu.in>
2026-03-07 19:11:50 +0530

    feat: Using ArrayInit

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,1 +1,17 @@
+/*
+ * @author: Dhruv Kumar Gupta
+ *
+ * @version: 1.0
+ */
 
+public class OOPSBannerApp_5 {
+    public static void main(String[] args) {
+        System.out.println(String.join("\n", new String[] {
+            "  ***   ***  ****  **** ",
+            " *   * *   * *   * *  ",
+            " *   * *   * ****  *** ",
+            " *   * *   * *       * ",
+            "  ***   ***  *    **** "
+        }));
+    }
+}
